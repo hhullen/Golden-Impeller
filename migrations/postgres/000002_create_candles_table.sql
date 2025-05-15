c3ac0daf5f4d1549e7d532eb10ed06b5e9f3b637
Maksim Lipatov <lipeinspecial@gmail.com>
2025-05-15 11:21:12 +0400

    changeg migration file

--- migrations/postgres/000002_create_candles_table.sql
+++ migrations/postgres/000002_create_candles_table.sql
@@ -1,34 +1,22 @@
 -- +goose Up
 -- +goose StatementBegin
 
-CREATE TYPE candle_interval AS ENUM (
-    '1min',
-    '2min',
-    '3min',
-    '5min',
-    '10min',
-    '15min',
-    '30min',
-    '1hour',
-    '2hour',
-    '4hour',
-    '1day',
-    '1week',
-    '1month'
-);
-
 CREATE TABLE IF NOT EXISTS candles (
     id SERIAL PRIMARY KEY,
     instrument_id INT NOT NULL REFERENCES instruments(id) ON DELETE CASCADE,
-    "date" TIMESTAMPTZ NOT NULL,
-    interval candle_interval NOT NULL,
-    "open" NUMERIC(15,9) NOT NULL,
-    "close" NUMERIC(15,9) NOT NULL,
-    high NUMERIC(15,9) NOT NULL,
-    low NUMERIC(15,9) NOT NULL,
+    "timestamp" TIMESTAMPTZ NOT NULL,
+    interval TEXT NOT NULL,
+    open_units BIGINT NOT NULL,
+    open_nano INT NOT NULL,
+    close_units BIGINT NOT NULL,
+    close_nano INT NOT NULL,
+    high_units BIGINT NOT NULL,
+    high_nano INT NOT NULL,
+    low_units BIGINT NOT NULL,
+    low_nano INT NOT NULL,
     volume BIGINT NOT NULL,
 
-    UNIQUE(instrument_id, "date", interval)
+    UNIQUE(instrument_id, "timestamp", interval)
 );
 
 -- +goose StatementEnd
